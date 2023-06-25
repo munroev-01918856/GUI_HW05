36ac51516cf339d0d3be51ce2705de6e6dd304ab
munroev-01918856 <104265619+munroev-01918856@users.noreply.github.com>
2023-06-24 20:26:42 -0400

    drag pract

--- script/main.js
+++ script/main.js
@@ -16,8 +16,14 @@ $(function() {
 
   var tilePool = [];
   var tileRack =[];
+  var score=0;
+  var counter=0;
 
-  
+  function initializeGame(){
+    fillTilePool();
+    loadRack()
+  }
+
   //Ajax to get json containing info about tiles and their distribution
   $.get("https://ykanane.github.io/Scrabble/pieces.json")
   .done(function(response) {
@@ -27,6 +33,7 @@ $(function() {
 
   //initialize the pool of tile with all duplicates included
   function fillTilePool(){
+    counter=0;
     console.log("intialize")
     for(i = 0; i < 27; i++){
       var currentTile = tileJSON[i];
@@ -43,7 +50,7 @@ $(function() {
       $(msg).text("Bag is empty");
       return;
     }
-    while(tileRack.length<7){
+    while(tileRack.length<7 && tilePool.length>0){
       randTile=Math.floor(Math.random() * tilePool.length);
       tileRack.push(tilePool[randTile]);
       delete tilePool[randTile];
@@ -55,43 +62,24 @@ $(function() {
     }
 }
   
-  function initializeGame(){
-    fillTilePool();
-    loadRack()
-  }
-  https://www.techiedelight.com/load-and-append-image-to-dom-javascript/
-  function loadTileGUI(tile, pos){
-  //   var image = new Image();
-  //   // image.width="300px"
-  //   // image.height="500px"
-  //  // imageasq21gbvb .setAttribute("class", "tile-img");
-  //   image.src ="a.jpeg"
-  //   $("#rack").append(image);
+ 
   
+  function loadTileGUI(tile, pos){
     var imageSrc= tileImg(tile.letter)
-    // // console.log(imageSrc);
-    var id="letter"+id;
+    var id="letter"+id+counter;
     var tileClass="tile";
-    
-
     $("#rack").append("<img src=\""+imageSrc+ "\""+
+    "draggable=\"true\""+
     "id=\""+id+ "\""+
     "class=\""+tileClass+ "\""+
     "value=\""+tile.value+ "\""+
     "letter=\""+tile.letter+ "\""+
+    
     "\">")
   }
 
 
-  //https://linuxhint.com/display-image-with-javascript/
-
-  // function displayImage(src, width, height) {
-  //   var img = document.createElement("img");
-  //   img.src = src;
-  //   img.width = width;
-  //   img.height = height;
-  //   document.body.appendChild(img);
-  //  }
+ 
 
   function tileImg(tile){
     var baseURL="images/Scrabble_Tiles/Scrabble_Tile_"
